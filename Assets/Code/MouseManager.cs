29e1f5ca8f41b9712a84e5664e71c9c9aa0a8d37
Willis <wfung19@hollyfield.kingston.sch.uk>
2025-09-20 19:57:07 +0100

    Added a line drawing system using points

---
+++ Assets/Code/MouseManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseManager : MonoBehaviour
+{
+    public Vector3 GetMousePosition()
+    {
+        Vector2 mousePos = Mouse.current.position.ReadValue(); // get mouse position in screen space
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, Camera.main.nearClipPlane)); // convert to world position
+        worldPos.z = 0f; // force z = 0 to keep in 2d
+
+        return worldPos;
+    }
+}
