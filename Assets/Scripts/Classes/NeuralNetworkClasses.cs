7b76c42a4d3f9195066fc83d483d2a59417a0cad
Willis <wfung19@hollyfield.kingston.sch.uk>
2026-02-18 01:10:51 +0000

    added obj refs in Connections, delegates, Caculate func in Node

--- Assets/Scripts/Classes/NeuralNetworkClasses.cs
+++ Assets/Scripts/Classes/NeuralNetworkClasses.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 public class NeuralNetwork {
@@ -5,15 +6,19 @@ public class NeuralNetwork {
     private List<Connection> connections = new List<Connection>();
 
     public NeuralNetwork(Genome genome) {
-        // logic to convert gene to functional nn stuff
+        // create Nodes and Connections from genes
+        // link them
     }
 
     public List<float> ForwardPass(List<float> inputs) 
     { 
-        return new List<float>(); // code later
+        // set innodes outputs
+        // calc hidden/ output nodes
+        return new List<float>(); 
     }
 }
 
+// Phenotype components
 public class Node {
     private int nodeID;
     private string nodeType;
@@ -25,9 +30,14 @@ public class Node {
     private float inputSum;
     private float output;
 
+    // getters
     public int NodeID => nodeID;
     public string NodeType => nodeType;
-    public float Output => output;
+    public float Output { get => output; set => output = value; }
+    public float InputSum { get => inputSum; set => inputSum = value; }
+
+    // store refs to conns feeding into this node
+    public List<Connection> IncomingConnections = new List<Connection>();
 
     public Node(NodeGene gene) 
     {
@@ -36,23 +46,58 @@ public class Node {
         this.bias = gene.bias;
         this.inputSum = 0f;
         this.output = 0f;
+
+        // map sting name from gene to actual math function
+        this.activationFunctionPtr = GetActivation(gene.activation);
+    }
+
+    public void Calculate()
+    {   // start with the defined bias
+        float sum = bias;
+
+        // sum up signals from enabled connections
+        foreach (var conn in IncomingConnections)
+        {
+            if (conn.Enabled)
+            {
+                sum += conn.InputNode.Output * conn.Weight;
+            }
+        }
+
+        inputSum = sum;
+        output = activationFunctionPtr(inputSum);
+    }
+
+    // helper to resolve delegates
+    private ActivationFunction GetActivation(string name) 
+    {
+        switch (name.ToLower()) {
+            case "sigmoid": return (x) => 1f / (1f + (float)Math.Exp(-x));
+            case "relu": return (x) => Math.Max(0, x);
+            case "linear": return (x) => x;
+            default: return (x) => (float)Math.Tanh(x);
+        }
     }
 }
 
 public class Connection {
-    private int inputNode;
-    private int outputNode;
+    // refs to node objs
+    private Node inputNode;
+    private Node outputNode;
+
     private float weight;
     private bool enabled;
 
     public float Weight => weight;
     public bool Enabled => enabled;
+    public Node InputNode => inputNode;
+    public Node OutputNode => outputNode;
 
-    public Connection(Node nodeA, Node nodeB, float w) 
+    public Connection(Node from, Node to, ConnectionGene gene) 
     {
-        this.inputNode = nodeA.NodeID;
-        this.outputNode = nodeB.NodeID;
-        this.weight = w;
-        this.enabled = true;
+        this.inputNode = from;
+        this.outputNode = to;
+        this.weight = gene.weight;
+        this.enabled = gene.enabled;
     }
 }
